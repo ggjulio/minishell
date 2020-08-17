5a84a0b8907aef019957212a7af55aad78fd7844
ggjulio <juligonz@student.42.fr>
2020-08-17 17:13:43 +0200

    test ft_printf on linux

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/20 13:32:17 by juligonz          #+#    #+#             */
-/*   Updated: 2020/08/16 16:58:17 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/08/17 17:13:21 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int	main(int ac, char **av, char **envp)
 	g_app = create_application(envp);
 	(void)ac;
 	(void)av;
-	printf("OKKKKkKKKeeeeeee\n");
+	ft_printf("OK\n");
 
 	char **tmp;
     for(tmp = envp; *tmp != 0; tmp++)
     {
         char *thisEnv = *tmp;
-        printf("%s\n", thisEnv);
+        ft_printf("%s\n", thisEnv);
     }
 
 	return (0);
