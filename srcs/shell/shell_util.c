18ae9b84099d117dbc80bfd636c62025e39d76aa
whayter <hwinston@student.42.fr>
2020-08-28 10:47:51 +0200

    ctrl d fixed

--- srcs/shell/shell_util.c
+++ srcs/shell/shell_util.c
@@ -6,7 +6,7 @@
 /*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 13:31:31 by juligonz          #+#    #+#             */
-/*   Updated: 2020/08/28 02:15:30 by hwinston         ###   ########.fr       */
+/*   Updated: 2020/08/28 10:47:17 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,12 @@ void	sig_handler(int sig)
 
 	(void)sig;
 	wait(&status);
+	// if (status == 3)
+	// {
+	// 	ft_dprintf(2, "Quit: 3\n");			// sortie standard ou error ?
+	// 	exit(EXIT_SUCCESS);
+	// }
+
 	if (status != SIGINT)
 	{
 		ft_printf("\n");
@@ -48,10 +54,12 @@ void	sig_handler(int sig)
 
 
 
+
 void	run_shell(void)
 {
 	char	*input;
 	t_list	*pipelines;
+	int		eof;
 
 	//g_sh.status = 1;
 	g_sh.pid = 1;
@@ -61,17 +69,13 @@ void	run_shell(void)
 	while (g_sh.status)
 	{
 		prompt_name();
-		get_next_line(STDIN_FILENO, &input);
+		if ((eof = get_next_line(STDIN_FILENO, &input)) == 0)
+			exit_shell(EXIT_SUCCESS);
 		if (input[0] != '\0')
 		{
 			pipelines = get_pipelines(input);
 			execute_pipelines(pipelines);
 			ft_lstclear(&pipelines, lst_del_command);
-		}
-		else
-		{
-			free(input);
-			exit_shell(EXIT_SUCCESS);
 		}
 		free(input);
 	}
