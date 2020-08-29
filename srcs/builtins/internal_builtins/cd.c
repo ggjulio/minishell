cc80a330f77138da27f556a12e09895cd88ff593
ggjulio <juligonz@student.42.fr>
2020-08-29 21:35:50 +0200

    fix cd

--- srcs/builtins/internal_builtins/cd.c
+++ srcs/builtins/internal_builtins/cd.c
@@ -3,25 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 13:40:40 by hwinston          #+#    #+#             */
-/*   Updated: 2020/08/29 16:40:37 by hwinston         ###   ########.fr       */
+/*   Updated: 2020/08/29 21:30:54 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int     cd(const char **args)
+int		cd(const char **args)
 {
-    if (args[1] == NULL)
-        return (0);
-    errno = 0;
-    if (chdir(args[1]) == -1)
-    {
-        error("cd", args[1]);
-        return (-1);
-    }
-    getcwd(g_sh.cwd, sizeof(g_sh.cwd));
-    return (0);
+	if (args[1] == NULL)
+		return (0);
+	errno = 0;
+	if (chdir(args[1]) == -1)
+	{
+		error("cd", args[1]);
+		return (-1);
+	}
+	set_environment_variable_value("OLDPWD", g_sh.cwd);
+	getcwd(g_sh.cwd, sizeof(g_sh.cwd));
+	set_environment_variable_value("PWD", g_sh.cwd);
+	return (0);
 }
