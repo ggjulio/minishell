d8ba15aa40a77374333f09d35209990852a08126
whayter <hwinston@student.42.fr>
2020-09-04 18:18:13 +0200

    fork internal builtins

--- srcs/command/command_spawn.c
+++ srcs/command/command_spawn.c
@@ -6,7 +6,7 @@
 /*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 13:45:43 by hwinston          #+#    #+#             */
-/*   Updated: 2020/09/04 16:12:17 by hwinston         ###   ########.fr       */
+/*   Updated: 2020/09/04 18:17:45 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,10 @@ int			run_command(t_command *command)
 {
 	t_builtin_ptr	builtin;
 
-	if ((builtin = get_builtin_ptr(command->args[0])) != NULL)
+	if ((builtin = get_internal_builtin_ptr(command->args[0])) != NULL \
+	&& command->pipe)
+		(*builtin)((const char **)command->args);
+	else if ((builtin = get_builtin_ptr(command->args[0])) != NULL)
 		(*builtin)((const char **)command->args);
 	else if (ft_strchr(command->args[0], '/') && !is_executable(command))
 		exit(EXIT_FAILURE);
@@ -55,25 +58,19 @@ int			fork_command(t_command *pipeline, int *pfd, int in)
 
 int			spawn_pipeline(t_command *pipeline)
 {
-	t_builtin_ptr	builtin;
 	int				pfd[2];
 	int				in;
 
 	in = 0;
 	while (pipeline)
 	{
-		if ((builtin = get_internal_builtin_ptr(pipeline->args[0])) \
-		!= NULL && !pipeline->pipe)
-			g_sh.status = (*builtin)((const char **)pipeline->args);
+
+		if (pipeline->redirections)
+			redirection_hub(pipeline, pipeline->redirections);
 		else
 		{
-			if (pipeline->redirections)
-				redirection_hub(pipeline, pipeline->redirections);
-			else
-			{
-				fork_command(pipeline, pfd, in);
-				in = pfd[0];
-			}
+			fork_command(pipeline, pfd, in);
+			in = pfd[0];
 		}
 		pipeline = pipeline->pipe;
 	}
