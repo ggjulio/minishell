a9b7149d8431a943436329128298fc9b7cf21d40
ggjulio <juligonz@student.42.fr>
2020-09-09 03:19:18 +0200

    fix echo

--- srcs/builtins/external_builtins/echo.c
+++ srcs/builtins/external_builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 13:40:40 by hwinston          #+#    #+#             */
-/*   Updated: 2020/09/07 13:31:03 by hwinston         ###   ########.fr       */
+/*   Updated: 2020/09/09 03:16:07 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,26 @@
 
 int		echo(const char **args)
 {
-	int opt;
+	int no_nl;
 	int i;
 
-	opt = (args[1] && ft_strcmp(args[1], "-n") == 0) ? 1 : 0;
-	if (args[1] == NULL && opt == 0)
+	if (args[1] == NULL)
 	{
 		ft_printf("\n");
 		exit(EXIT_SUCCESS);
 		return (0);
 	}
-	i = 0 + opt;
+	no_nl = 1;
+	while (args[no_nl] && !ft_strcmp(args[no_nl], "-n"))
+		no_nl++;
+	i = no_nl - 1;
 	while (args[++i])
 	{
-		if ((opt == 0 && i > 1) || (opt == 1 && i > 2))
-			ft_printf(" ");
 		ft_printf("%s", args[i]);
+		if (args[i + 1])
+			ft_printf(" ");
 	}
-	if (opt == 0)
+	if (no_nl <= 1)
 		ft_printf("\n");
 	exit(EXIT_SUCCESS);
 	return (0);
