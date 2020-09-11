0e4ecc4a1a7e6aace520bd600a089c9fc3574833
ggjulio <juligonz@student.42.fr>
2020-09-11 21:34:51 +0200

    fix type in error.c (Is a directory)

--- srcs/error/error.c
+++ srcs/error/error.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 13:40:40 by hwinston          #+#    #+#             */
-/*   Updated: 2020/09/11 01:07:55 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/09/11 21:34:11 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void		error(char *cmd, const char *arg)
 	if (arg[0] != '\0')
 		ft_dprintf(STDERR_FILENO, "%s: ", arg);
 	ft_dprintf(STDERR_FILENO, "%s\n", strerror(errno));
-	// g_sh.status = STATUS_CMD_NOT_FOUND;
 }
 
 void		bad_command(char *cmd)
@@ -34,7 +33,7 @@ int			permission_error(char *command, int option)
 	ft_dprintf(STDERR_FILENO, "%s: ", g_sh.name);
 	ft_dprintf(STDERR_FILENO, "%s: ", command);
 	if (option == 1)
-		ft_dprintf(STDERR_FILENO, "is a directory\n");
+		ft_dprintf(STDERR_FILENO, "Is a directory\n");
 	else if (option == 2)
 		ft_dprintf(STDERR_FILENO, "Permission denied\n");
 	g_sh.status = STATUS_NOT_EXECUTABLE;
